3cdc89e3fb9b28cd2eaec5b957259123f81c0714
exile213 <exilegamer3@gmail.com>
2023-08-09 00:08:47 +0800

    Update playRound Function by showing Player and Computer Choices

--- script.js
+++ script.js
@@ -15,27 +15,35 @@ function game(){
 
         // Player wins
         if(playerSelection === "rock" &&  computerSelection === "scissors"){
+            console.log("Player: " + playerSelection + "  Computer: " + computerSelection );
             return "You Win! Rock beats Scissors";
         }
         else if(playerSelection === "paper" &&  computerSelection === "rock"){
+            console.log("Player: " + playerSelection + "  Computer: " + computerSelection );
             return "You Win! Paper beats Rock";
         }
         else if(playerSelection === "scissors" &&  computerSelection === "paper"){
+            console.log("Player: " + playerSelection + "  Computer: " + computerSelection );
             return "You Win! Scissors beats Paper";
         }
         
         //computer wins
         else if(playerSelection === "rock" &&  computerSelection === "paper"){
+            console.log("Player: " + playerSelection + "  Computer: " + computerSelection );
             return "You lose! Paper beats Rock";
         }
         else if(playerSelection === "paper" &&  computerSelection === "scissors"){
+            console.log("Player: " + playerSelection + "  Computer: " + computerSelection );
             return "You lose! Scissors beats Paper";
         }
         else if(playerSelection === "scissors" &&  computerSelection === "rock"){
+            console.log("Player: " + playerSelection + "  Computer: " + computerSelection );
             return "You lose! Rock beats Scissors";
         }
 
+        //tie
         else{
+            console.log("Player: " + playerSelection + "  Computer: " + computerSelection );
             return "You tie with the Computer!";
         }
     }
@@ -43,3 +51,5 @@ function game(){
     console.log(playRound(playerSelection, computerSelection));
 
 }
+
+game();
