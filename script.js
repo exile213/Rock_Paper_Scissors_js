88e4354b9de546c122d9f25d5c328d67d6c40b5c
exile213 <exilegamer3@gmail.com>
2023-08-08 22:48:08 +0800

    Add given JS Functions

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function game(){
+    const playerSelection = "rock";
+    const computerSelection = getComputerChoice();
+    
+    
+    function getComputerChoice(CompChoice){
+    
+    }
+    
+    function playRound(playerSelection, computerSelection) {
+        // your code here!
+    }
+       
+
+
+    console.log(playRound(playerSelection, computerSelection));
+    
+    
+
+
+}
