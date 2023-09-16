13d15eb231a73b230dfcaee85ef683db83948b25
exile213 <exilegamer3@gmail.com>
2023-09-16 23:43:57 +0800

    Add Battle button functionality, Add Computer Randomization UI Responsiveness

--- script.js
+++ script.js
@@ -3,20 +3,19 @@ let CompScore=0;
 
 function game(){
         //Variable Section ----------------------------------------------------------------
-        /*
+    
         let playerSelection; 
-        let computerSelection = getComputerChoice();
-        */
+        let computerSelection = randComputerChoice();
 
         //PLAYER CHOICE BUTTONS Variables
         const rockBtn = document.getElementById("Rock");
         const paperBtn =document.getElementById("Paper");
         const scissorsBtn =document.getElementById("Scissors");
-
+        const battleBtn = document.getElementById("Battle");
 
         //Player Selected Choice variable
         let plChose= document.getElementById("playerChose");
-
+        let compChose = document.getElementById("computerChose");
 
 
         //Player Clicks Choice Buttons events
@@ -31,32 +30,50 @@ function game(){
         //Player Selects Rock Function
         function plSelectRock(){
             plChose.className="playerChoseRock";
-            console.log(plChose);
+            playerSelection="rock";
         }
 
         //Player Selects Paper Function
         function plSelectPaper(){
             plChose.className="playerChosePaper";
+            playerSelection="paper";
         }
 
         //Player Selects Rock Function
         function  plSelectScissors(){
             plChose.className="playerChoseScissors";
+            playerSelection="scissors";
         }
-
 
 
         //Computer Randomizer Function
-        function getComputerChoice(){
+        function randComputerChoice(){
             const compChoices = ["rock", "paper", "scissors"];
             const compRand = Math.floor(Math.random() * 3);
-    
             return compChoices[compRand];
         }
 
-/*
+
+
+
+        //Player clicks battle button, calls round start function
+        battleBtn.addEventListener("click",function() {
+            playRound(playerSelection, computerSelection);
+        })
+
         //Round Start Function
         function playRound(playerSelection, computerSelection) {
+
+            //Shows what the Computer Chose in UI
+                if(computerSelection === "scissors"){
+                    compChose.className = "computerChoseScissors";
+                }
+                else if(computerSelection === "paper"){
+                    compChose.className = "computerChosePaper";
+                }
+                else if(computerSelection === "rock"){
+                    compChose.className = "computerChoseRock";
+                }
 
 
             // Player wins
@@ -108,8 +125,6 @@ function game(){
             CompScore = CompScore+1;
         }
         
-        console.log(playRound(playerSelection, computerSelection));
-    */
     }
 
     /*
